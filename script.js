0a14cad1cd248e847dde28bf8999e017ad596b4a
TokyoProgramming <samuraidjango.jp@gmail.com>
2020-12-31 21:14:14 +0900

    add post function

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ const postArticle = document.getElementById('inputPost');
 const postButton = document.getElementById('user-button');
 const btn = document.getElementById('user-button');
 const main = document.querySelector('.main');
+const postCard = document.querySelector('.postCard');
 
 let posts = [];
+let userPostsArray = [];
 
 // getRandomUser Picture
 const getRandomUser = async () => {
@@ -27,7 +29,6 @@ const getRandomUser = async () => {
       userPic: `${e.picture.large}`,
     });
   });
-  console.log(myArray);
 
   return myArray;
 };
@@ -39,13 +40,12 @@ const getRandomPosts = async () => {
   const postsData = await postsRes.json();
 
   //   get first 10 posts
-  let posts = [];
+
   for (let i = 0; i < 10; i++) {
     posts.push(postsData[i]);
   }
 
   // Make userPosts Array
-  let userPostsArray = [];
 
   for (let i = 0; i < 10; i++) {
     const id = userInfo[i].userId;
@@ -103,7 +103,7 @@ const getRandomPosts = async () => {
 
     const postInfPostIconsDiv = document.createElement('div');
     postInfPostIconsDiv.classList.add('post-icons');
-    postInfPostIconsDiv.innerHTML = `                <i class="far fa-comment"></i>
+    postInfPostIconsDiv.innerHTML = `<i class="far fa-comment"></i>
 	<i class="fas fa-redo-alt"></i>
 	<i class="far fa-heart"></i>
 	<i class="far fa-save"></i>`;
@@ -113,21 +113,86 @@ const getRandomPosts = async () => {
     postInfDiv.appendChild(postInfPostDiv);
     postCardDiv.appendChild(postInfDiv);
 
-    main.appendChild(postCardDiv);
+    postCard.appendChild(postCardDiv);
   });
-  //   console.log(mainDiv);
 };
 
 // Post Article
 const postBtn = (e) => {
   e.preventDefault();
+  console.log(userPostsArray);
 
   const article = postArticle.value;
 
-  posts.push(article);
-  console.log(article);
-
   postArticle.value = '';
+  const id = 'sampleId';
+  const name = 'sampleUserName';
+  const post = article;
+  const userPic = `https://randomuser.me/api/portraits/men/22.jpg`;
+
+  //   Write HTML
+  // Create PostCardDiv
+  const postCardDiv = document.createElement('div');
+  postCardDiv.classList.add('post-card');
+  // Add Image
+  const postImage = document.createElement('img');
+  postImage.src = `${userPic}`;
+  postCardDiv.appendChild(postImage);
+  // Create PostInfDiv
+  const postInfDiv = document.createElement('div');
+  postInfDiv.classList.add('post-inf');
+  // create user-inf div
+  const userInfDiv = document.createElement('div');
+  userInfDiv.classList.add('user-inf');
+  // create h3
+  const postInfH3 = document.createElement('h3');
+  postInfH3.classList.add('user-name');
+  // h3 InnerText UserName
+  postInfH3.innerText = `${name}`;
+  // create h5
+  const postInfH5 = document.createElement('h5');
+  postInfH5.classList.add('user-id');
+  // h5 InnerText UserID
+  postInfH5.innerText = `${id}`;
+
+  userInfDiv.appendChild(postInfH3);
+  userInfDiv.appendChild(postInfH5);
+  postInfDiv.appendChild(userInfDiv);
+
+  // Create Post Div
+  const postInfPostDiv = document.createElement('div');
+  postInfPostDiv.classList.add('post');
+  // Create Post P
+  const postInfP = document.createElement('p');
+  // post InnerText Post
+  postInfP.innerText = `${post}`;
+  // Append PostInfP to post Div
+  postInfPostDiv.appendChild(postInfP);
+
+  const postInfPostIconsDiv = document.createElement('div');
+  postInfPostIconsDiv.classList.add('post-icons');
+  postInfPostIconsDiv.innerHTML = `<i class="far fa-comment"></i>
+	 <i class="fas fa-redo-alt"></i>
+	 <i class="far fa-heart"></i>
+	 <i class="far fa-save"></i>`;
+
+  postInfPostDiv.appendChild(postInfPostIconsDiv);
+
+  postInfDiv.appendChild(postInfPostDiv);
+  postCardDiv.appendChild(postInfDiv);
+
+  postCard.prepend(postCardDiv);
+
+  // END HTML
+
+  (createdPost = {
+    id,
+    name,
+    post,
+    userPic,
+  }),
+    userPostsArray.unshift(createdPost);
+  console.log(userPostsArray);
 };
 
 // Change Btn to Active
